50a43460c1e1d3479ed9b740124896089c8cfa14
pengyouwei6717 <pengyouwei6717@ipalfish.com>
2020-05-19 15:06:38 +0800

    add

---
+++ src/components/button/button.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="test">
+    {{count}}
+    <button @click="handleAdd">add</button>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+export default {
+  setup() {
+    const count = ref(0)
+    const handleAdd = () => {
+      count.value++
+    }
+    return {
+      count,
+      handleAdd
+    }
+  }
+}
+</script>
